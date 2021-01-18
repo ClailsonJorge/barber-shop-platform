169f46a3a10d759e9a9dd2d887e88a967094ddb8
ClailsonJorge <clailsonjorge@gmail.com>
2021-01-17 21:38:25 -0300

    refactor: move code to appointmentsRepository

---
+++ nodejs/src/repsitories/appointmentsRepository.ts
@@ -0,0 +1,24 @@
+import { isEqual } from 'date-fns'
+import Appointment from '../models/appointment'
+
+class AppointmentRepository {
+    private appointments: Appointment[]
+
+    constructor() {
+        this.appointments = []
+    }
+
+    public findByDate(date: Date): Appointment | null {
+        const findAppointmentsEquals = this.appointments.find((appointment) =>
+            isEqual(date, appointment.date)
+        )
+        return findAppointmentsEquals || null
+    }
+
+    public create(provider: string, date: Date): Appointment | null {
+        const appointment = new Appointment(provider, date)
+        this.appointments.push(appointment)
+        return appointment
+    }
+}
+export default AppointmentRepository
--- nodejs/src/routes/appointments.routes.ts
+++ nodejs/src/routes/appointments.routes.ts
@@ -1,25 +1,22 @@
 import { Router } from 'express'
-import { startOfHour, parseISO, isEqual } from 'date-fns'
-import Appointment from '../models/appointment'
+import { startOfHour, parseISO } from 'date-fns'
+import AppointmentRepository from '../repsitories/appointmentsRepository'
 
 const appointmentsRouter = Router()
 
-const appointments: Appointment[] = []
+const appointments = new AppointmentRepository()
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body
     const parseDate = startOfHour(parseISO(date))
-    const findAppointmentsEquals = appointments.find((appointment) =>
-        isEqual(parseDate, appointment.date)
-    )
+    const findAppointmentsEquals = appointments.findByDate(parseDate)
 
     if (findAppointmentsEquals) {
         return response.json({
             message: 'This appointments is already booked'
         })
     }
-    const appointment = new Appointment(provider, parseDate)
-    appointments.push(appointment)
+    const appointment = appointments.create(provider, parseDate)
     return response.json(appointment)
 })
 
