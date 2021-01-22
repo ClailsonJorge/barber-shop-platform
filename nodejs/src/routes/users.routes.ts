2174980dea9d0f0db472619dd9d29190f6c62eba
ClailsonJorge <clailsonjorge@gmail.com>
2021-01-22 20:09:14 -0300

    feat: added patch route to upload avatar file

--- nodejs/src/routes/users.routes.ts
+++ nodejs/src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
+import multer from 'multer'
 import { CreateUserService } from '../services'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import uploadonfig from '../config/upload'
 
 const usersRouter = Router()
+const upload = multer(uploadonfig)
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -14,4 +18,13 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ message: error.message })
     }
 })
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        return response.json({ ok: true })
+    }
+)
 export default usersRouter
