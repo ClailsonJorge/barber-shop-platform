c223c7dac66f3f64c9ab82c9580dead4f38ff9a9
ClailsonJorge <clailsonjorge@gmail.com>
2021-01-22 21:20:32 -0300

    feat: created route to send img file

--- nodejs/src/server.ts
+++ nodejs/src/server.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata'
 import './database'
 import express from 'express'
 import routes from './routes'
+import uploadConfig from './config/upload'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use('/files', express.static(uploadConfig.directory))
 
 app.listen(3333, () => {
     console.log('Server initialing... ')
