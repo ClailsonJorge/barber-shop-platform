dab6b3e764c1c59c6288af72fa9682815bb4e546
ClailsonJorge <clailsonjorge@gmail.com>
2021-01-29 21:30:01 -0300

    chore: corected configure lint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from 'react'
+import GlobalStyle from './styles/global'
 
 function App() {
-  return (<h1>Hello World</h1>);
+    return (
+        <>
+            <h1>Hello World</h1>
+            <GlobalStyle />
+        </>
+    )
 }
-
-export default App;
+export default App
