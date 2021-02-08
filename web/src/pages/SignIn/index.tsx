757eb27019c05ad74b40e1ea4024faf76b428d82
ClailsonJorge <clailsonjorge@gmail.com>
2021-02-08 12:37:33 -0300

    feat: ensure authentication of SingIn

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web'
+import * as Yup from 'yup'
+import { FormHandles } from '@unform/core'
 import { Background, Container, Content } from './styles'
 import logoImg from '../../assets/LogoGobarber.svg'
 import Input from '../../Components/input'
 import Button from '../../Components/button'
+import getValidationErrors from '../../utils/getValidationErrors'
 
 const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null)
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            formRef.current?.setErrors({})
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Name incorrect'),
+                email: Yup.string()
+                    .required('E-mail is necessary')
+                    .email('The email format is not correct.'),
+                password: Yup.string().required('Password is necessary.')
+            })
+
+            await schema.validate(data, {
+                abortEarly: false
+            })
+        } catch (err) {
+            formRef.current?.setErrors(getValidationErrors(err))
+        }
+    }, [])
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="GoBarber" />
-                <form>
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Faça seu login</h1>
                     <Input
                         icon={FiMail}
@@ -26,7 +49,7 @@ const SignIn: React.FC = () => {
                     />
                     <Button type="submit">Entrar</Button>
                     <a href="#teste">Esqueci minha senha</a>
-                </form>
+                </Form>
 
                 <a href="#teste">
                     <FiLogIn />
