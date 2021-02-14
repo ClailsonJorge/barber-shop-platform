9181dd3b7a45277065dda6f79daeba105876be0a
ClailsonJorge <clailsonjorge@gmail.com>
2021-02-14 16:43:42 -0300

    feat: added private route

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-
+import { Switch } from 'react-router-dom'
+import Dashboard from '../pages/dashboard'
+import Route from './route'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 
@@ -9,6 +10,8 @@ const Routes: React.FC = () => {
         <Switch>
             <Route path="/" exact component={SignIn} />
             <Route path="/signup" component={SignUp} />
+
+            <Route path="/dashboard" component={Dashboard} isPrivate />
         </Switch>
     )
 }
