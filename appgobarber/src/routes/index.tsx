a9e89afdf2f63848b52fc6a6c8a6c2dbd8c897ce
ClailsonJorge <clailsonjorge@gmail.com>
2021-02-22 21:57:43 -0300

    chore: first commit

---
+++ appgobarber/src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import SignIn from '../pages/signin'
+import SignUp from '../pages/signup'
+
+const Auth = createStackNavigator()
+
+const Route: React.FC = () => {
+    return (
+        <Auth.Navigator
+         screenOptions={{
+             headerShown: false,
+             cardStyle: {backgroundColor: '#312e38'}
+         }}
+        >
+            <Auth.Screen name="signin" component={SignIn} />
+            <Auth.Screen name="signup" component={SignUp} />
+        </Auth.Navigator>
+    )
+}
+
+export default Route
