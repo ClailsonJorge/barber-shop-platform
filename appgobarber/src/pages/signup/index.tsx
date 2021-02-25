89932287dbf2ba821619d79fe9098c94e78747ed
ClailsonJorge <clailsonjorge@gmail.com>
2021-02-25 12:13:22 -0300

    feat: development signup

--- appgobarber/src/pages/signup/index.tsx
+++ appgobarber/src/pages/signup/index.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import { Container } from './styles'
+import React, { useEffect, useState } from 'react'
+import { Image, ScrollView, KeyboardAvoidingView, Platform, Keyboard } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
+import logo from '../../assets/logo.png'
+import Button from '../../components/button'
+import Input from '../../components/input'
 
+import { Container, Title, CreateAccount, CreateAccountText } from './styles'
 
-const SignUp: React.FC = () => {
+const SignIn: React.FC = () => {
+    const [keyboard, setKeyboard] = useState(false)
+    const navigation = useNavigation()
+
+    const handleKeyBoardShow = () => {
+        setKeyboard(true);
+    }
+
+    const handleKeyBoardHide = () => {
+        setKeyboard(false);
+    }
+
+    useEffect(()=>{
+        Keyboard.addListener('keyboardDidShow', handleKeyBoardShow)
+        Keyboard.addListener('keyboardDidHide', handleKeyBoardHide)
+
+        return () => {
+            Keyboard.removeListener('keyboardWillShow', handleKeyBoardShow)
+            Keyboard.removeListener('keyboardDidHide', handleKeyBoardHide)
+        }
+    },[])
+
     return (
-        <Container />
+        <>
+            <KeyboardAvoidingView style={{flex: 1,}} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+                <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{flex: 1,}}>
+                    <Container>
+                        <Image source={logo} />
+                        <Title>Crie sua conta</Title>
+                        <Input name="name" icon="user" placeholder="Nome"/>
+                        <Input name="email" icon="mail" placeholder="E-mail"/>
+                        <Input name="password" icon="lock" placeholder="Senha"/>
+                        <Button>Cadastrar</Button>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+
+            {!keyboard && <CreateAccount keyboard={keyboard} onPress={() => navigation.goBack()}>
+                <Icon name="arrow-left" size={20} color="#fff"/>
+                <CreateAccountText>Voltar para o login</CreateAccountText>
+            </CreateAccount>}
+        </>
     )
 }
 
-export default SignUp
+export default SignIn
+
--- appgobarber/src/pages/signup/styles.ts
+++ appgobarber/src/pages/signup/styles.ts
@@ -1,3 +1,45 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View``
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    padding: 0 40px;
+`
+
+export const Title = styled.Text`
+    font-family: 'RobotoSlab-Medium';
+    font-size: 20px;
+    line-height: 24px;
+    color: #F4EDE8;
+    margin: 64px 0 24px;
+`
+
+interface CreateAccountProps {
+    keyboard: boolean
+}
+
+export const CreateAccount = styled.TouchableOpacity<CreateAccountProps>`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+
+    flex-direction: row;
+    width: 100%;
+    height: 54px;
+    border-top-width: 1px;
+    border-top-color: #232129;
+
+    justify-content: center;
+    align-items: center;
+`
+
+export const CreateAccountText = styled.Text`
+    font-family: 'RobotoSlab-Regular';
+    font-size: 16px;
+    color: #fff;
+    line-height: 18px;
+    margin-left: 18px;
+`
