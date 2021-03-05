82ff0d0b258cd52191045dbd70a19c5caee80e19
ClailsonJorge <clailsonjorge@gmail.com>
2021-03-05 10:19:35 -0300

    chore: restructuring folders like separates modules

--- nodejs/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AuthenticateUserService } from './authenticationUserService'
-export { default as CreateAppointment } from './createAppointmentService'
-export { default as CreateUserService } from './createUserService'
